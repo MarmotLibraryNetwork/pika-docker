cf277be7bc18b9232c4e5a1daad06c6f64eea83f
Chris <chris@marmot.org>
2021-06-09 15:44:03 -0500

    initial commit

---
+++ html/index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title></title>
+  <meta name="description" content="">
+
+  <!-- link rel="stylesheet" href="css/styles.css" -->
+</head>
+
+<body>
+<h1>Docker Apache, PHP, Solr, MariaDB</h1>
+<p><a href="phpinfo.php">PHP Info</a></p>
+<p><a href="http://localhost:8983/solr/#/">Solr</a></p>
+  <!-- script src="js/scripts.js"></script -->
+</body>
+</html>
+
